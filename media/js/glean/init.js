7aab3cc464b7db519bec4380a4bd20603c4b36e1
brizental <beatriz.rizental@gmail.com>
2021-11-16 18:05:24 +0100

    Glean.js bedrock demo

--- media/js/glean/init.js
+++ media/js/glean/init.js
@@ -1,1 +1,12 @@
+import Glean from "@mozilla/glean/web";
+
+import { pageView as pageViewPing } from "./generated/pings.js";
+import * as pageMetrics from "./generated/page.js";
+
+Glean.setLogPings(true);
+Glean.initialize("moz-bedrock", true);
+
+pageMetrics.viewed.set();
+pageMetrics.id.set(Mozilla.Analytics.getPageId(location.pathname));
+pageViewPing.submit();
 
